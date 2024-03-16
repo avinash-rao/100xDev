c0df42abf860667114177bf638daf721c8c8f230
Personal - Avinash Rao <avinash_it@yahoo.com>
2024-03-16 16:33:03 +0530

    week 8 and week 9

--- week-8/paytm/backend/index.js
+++ week-8/paytm/backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const mainRouter = require('routes/index')
 
+const app = express()
 
+app.use('/api/v1', mainRouter)
+app.use(cors({
+    // specify frontend url for cors
+    origin: ''
+}))
+
+app.use(express.json())
+
+app.listen(() => {
+    console.log('App Started in port 3000');
+})
